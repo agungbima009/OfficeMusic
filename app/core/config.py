15650a42e52b85f9913ccf5a77ea21cb38dd9dd0
agungbima009 <agungbima132@gmail.com>
2026-05-22 19:06:46 +0700

    pp

--- app/core/config.py
+++ app/core/config.py
@@ -4,6 +4,10 @@ import os
 load_dotenv()
 
 API_KEY = os.getenv("YOUR_API_KEY")
+BACKEND_URL = os.getenv("BACKEND_URL")
 
 if not API_KEY:
     raise ValueError("YouTube API KEY not found")
+
+if not BACKEND_URL:
+    raise ValueError("Backend URL not found")
